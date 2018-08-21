cc0e7b8c3f507042821279234696793fb5f88d6a
Loi Tran <tdloi@protonmail.com>
2018-08-22 01:55:43 +0700

    Update Leaflet to add coordinate from input to map

--- script.js
+++ script.js
@@ -9,8 +9,21 @@ function getDistance(lat1, lon1, lat2, lon2) {
     ;
   const c = Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
   return 2 * R * c;
+
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
 }
 
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
+function unpackCoordinates(rawValues) {
+  let listCoordinates = rawValues.split(' ');
+  listCoordinates = listCoordinates.filter(value => value !== "");
+  return [{
+    "lat": listCoordinates[0],
+    "lon": listCoordinates[1]
+  }, {
+    "lat": listCoordinates[2],
+    "lon": listCoordinates[3]
+  }];
 }
+
