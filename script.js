a5953c349c943c9f4ca5b9cd5b5405e00f1fbf2b
Loi Tran <tdloi@protonmail.com>
2018-08-22 12:04:18 +0700

    Move event handle to javascript files

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ function unpackCoordinates(rawValues) {
   }];
 }
 
+(function buttonProceedOnClick() {
+  document.getElementById('proceed-button').onclick = function() {
+    let listCoordinates = document.getElementById('list-coordinates');
+    listCoordinates.value = listCoordinates.value.replace(/\t/g, ' ');
+    let listMarker = listCoordinates.value.split('\n').reduce(
+      (accur, curr) => accur.concat(unpackCoordinates(curr)),
+      []
+    ).filter(coordinate =>
+        coordinate.lat !== undefined && coordinate.lon !== undefined
+    );
+
+    for (let coordinate of listMarker) {
+      L.marker([coordinate.lat, coordinate.lon]).addTo(map);
+    }
+    map.panTo(new L.LatLng(listMarker[0].lat, listMarker[0].lon));
+  }
+})();
