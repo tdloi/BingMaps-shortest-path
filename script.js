c519a8fe57c2cc25efb3561f87297b5e52d397d4
Loi Tran <tdloi@protonmail.com>
2018-08-22 14:14:33 +0700

    Replace onclick with addEvenListener

--- script.js
+++ script.js
@@ -27,20 +27,23 @@ function unpackCoordinates(rawValues) {
   }];
 }
 
-(function buttonProceedOnClick() {
-  document.getElementById('proceed-button').onclick = function() {
-    let listCoordinates = document.getElementById('list-coordinates');
-    listCoordinates.value = listCoordinates.value.replace(/\t/g, ' ');
-    let listMarker = listCoordinates.value.split('\n').reduce(
-      (accur, curr) => accur.concat(unpackCoordinates(curr)),
-      []
-    ).filter(coordinate =>
-        coordinate.lat !== undefined && coordinate.lon !== undefined
-    );
-
-    for (let coordinate of listMarker) {
-      L.marker([coordinate.lat, coordinate.lon]).addTo(map);
-    }
-    map.panTo(new L.LatLng(listMarker[0].lat, listMarker[0].lon));
-  }
+(function addButtonProceedEventHandle() {
+  let buttonProceed = document.getElementById('proceed-button');
+  buttonProceed.addEventListener('click', addMarkerToMap);
 })();
+
+function addMarkerToMap() {
+  let listCoordinates = document.getElementById('list-coordinates');
+  listCoordinates.value = listCoordinates.value.replace(/\t/g, ' ');
+  let listMarker = listCoordinates.value.split('\n').reduce(
+    (accur, curr) => accur.concat(unpackCoordinates(curr)),
+    []
+  ).filter(coordinate =>
+      coordinate.lat !== undefined && coordinate.lon !== undefined
+  );
+
+  for (let coordinate of listMarker) {
+    L.marker([coordinate.lat, coordinate.lon]).addTo(map);
+  }
+  map.panTo(new L.LatLng(listMarker[0].lat, listMarker[0].lon));
+}
