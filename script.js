81f6138489ed644c0c7e1f938e6d1e4dbd4d5b7a
Loi Tran <tdloi@protonmail.com>
2018-08-24 15:09:58 +0700

    Update addMarkerToMap method
    Marker will be added into layerGroup instead of map, the
    purpose of this is to remove old marker when adding new
    marker.
    
    Coordinate will be compared as string to remove duplicates,
    then convert to object to add to map

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function unpackCoordinates(rawValues) {
-  let listCoordinates = rawValues.split(' ');
+function splitPairCoordinate(pairCoordinate) {
+  let listCoordinates = pairCoordinate.split(' ');
   listCoordinates = listCoordinates.filter(value => value !== "");
+  return [
+    `${listCoordinates[0]} ${listCoordinates[1]}`,
+    `${listCoordinates[2]} ${listCoordinates[3]}`
+  ];
+}
+
+function convertStringCoordinateToObject(stringCoordinate) {
+  let [lat, lon] = stringCoordinate.split(' ');
   return [{
-    "lat": listCoordinates[0],
-    "lon": listCoordinates[1]
-  }, {
-    "lat": listCoordinates[2],
-    "lon": listCoordinates[3]
+    "lat": lat,
+    "lon": lon,
   }];
 }
 
@@ -18,16 +23,24 @@ function unpackCoordinates(rawValues) {
 function addMarkerToMap() {
   let listCoordinates = document.getElementById('list-coordinates');
   listCoordinates.value = listCoordinates.value.replace(/\t/g, ' ');
-  let listMarker = listCoordinates.value.split('\n').reduce(
-    (accur, curr) => accur.concat(unpackCoordinates(curr)),
+  let uniqueCoordinateString = listCoordinates.value.split('\n').reduce(
+    (accur, curr) => accur.concat(splitPairCoordinate(curr)),
     []
-  ).filter(coordinate =>
-      coordinate.lat !== undefined && coordinate.lon !== undefined
+  ).filter(
+    (curr, index, self) => self.indexOf(curr) === index
+  );
+  let listMarker = uniqueCoordinateString.reduce(
+    (acc, curr) => acc.concat(convertStringCoordinateToObject(curr)),
+    []
+  ).filter(
+    coordinate => coordinate.lat !== 'undefined' && coordinate.lon !== 'undefined'
   );
 
+  markerGroup.clearLayers();
   for (let coordinate of listMarker) {
-    L.marker([coordinate.lat, coordinate.lon]).addTo(map);
+    L.marker([coordinate.lat, coordinate.lon]).addTo(markerGroup);
   }
+  markerGroup.addTo(map);
   map.panTo(new L.LatLng(listMarker[0].lat, listMarker[0].lon));
 }
 
