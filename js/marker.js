c673090f97aea1d5230f9341c352b9a1d0decafb
Loi Tran <tdloi@protonmail.com>
2019-02-05 00:36:14 +0700

    Add drawPolyline for marker module

--- js/marker.js
+++ js/marker.js
@@ -1,14 +1,15 @@
 export const Marker = {
-  /**
-   * Add a marker to FeatureGroup then add it to map
-   * @param {Coordinate} marker An instance of Coordinate class
-   * @param {FeatureGroup} group featureGroup which marker belongs to
-   */
+  // Add a marker to FeatureGroup then add it to map
   addToMap: function(marker, group, iconPath='') {
+    let popup = `<strong>${marker.name}</strong><br>
+                 Lat: ${marker.lat}<br>
+                 Lon: ${marker.lon}`;
+    if (marker.ele !== null) {
+      popup += `<br>Elevation: ${marker.ele} m`;
+    }
+
     L.marker([marker.lat, marker.lon]).addTo(group)
-      .bindPopup(`<strong>${marker.name}</strong><br>
-                  Elevation: ${marker.ele} m <br>
-                  ${marker.lat}, ${marker.lon}`);
+      .bindPopup(popup);
     if (iconPath !== '') {
       if (iconPath === 'red') iconPath = "img/marker-red.png";
       // Change iconUrl will result in relative path of Leaflet unpkg path
@@ -23,22 +24,38 @@ export const Marker = {
     group.addTo(map);
   },
 
-  /**
-   * Bind and open popup of a marker in map.
-   * @param {Coordinate} marker An instance of Coordinate class
-   * @param {FeatureGroup} group featureGroup which marker belongs to
-   */
-  openPopup: function (marker, group) {
+  // Bind and open popup of a marker in map.
+  openPopup: function(marker, group) {
     // Currently there are no way to open a binded popup
     // so we need to re-bind it then use openPopup method
+    let popup = `<strong>${marker.name}</strong><br>
+                 Lat: ${marker.lat}<br>
+                 Lon: ${marker.lon}`;
+    if (marker.ele !== null) {
+      popup += `<br>Elevation: ${marker.ele} m`;
+    }
+
     L.marker([marker.lat, marker.lon]).addTo(group)
-      .bindPopup(`<strong>${marker.name}</strong><br>
-                  Elevation: ${marker.ele} m <br>
-                  ${marker.lat}, ${marker.lon}`)
+      .bindPopup(popup)
       .openPopup();
   },
 
-  panTo: function (marker) {
+  panTo: function(marker) {
     map.panTo(new L.LatLng(marker.lat, marker.lon));
+  },
+
+  drawPolyline: function(latlongs, color='#0e6dd7', group) {
+    // Draw polyline from array of latlongs
+    // latlongs can be either one or multi-dimensions
+    // [ [x1, y1],
+    //   [x2, y2], ]
+    // or
+    // [
+    //   [[x1, y1],
+    //    [x2, y2], ],
+    //   [[a1, b1],
+    //    [a2, b2], ],
+    // ]
+    L.polyline(latlongs, {color: color}).addTo(group);
   },
 };
